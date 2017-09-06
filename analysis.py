53d5fb582a24eaf225f032cecd9e6e5f6e2c1bbc
Ryan Cloutier <cloutier@astro.utoronto.ca>
2017-09-06 15:06:45 -0400

    wrote new plotting function to deal with the summed lnlikelihood in addition to the median

--- analysis.py
+++ analysis.py
@@ -4,19 +4,26 @@ from savepickle import *
 
 def MAD(arr):
     return np.median(abs(arr - np.median(arr)))
-    
+
+
+def get_results(datanum):
+    pickles = np.array(glob.glob('results/RVdata%.4d/qsub_*'%datanum))
+    pickles = np.sort(pickles)
+    selfs = [loadpickle(pickles[i]) for i in range(pickles.size)]
+    return selfs
+
 
 def get_median_results(datanum):
     # Get results for this dataset
-    pickles = np.array(glob.glob('results/RVdata%.4d/qsub_*'%datanum))
-    nmodels = pickles.size
-
+    selfs = get_results(datanum)
+    nmodels = len(selfs)
+    
     # Save median lnlikelihoods
     medians, mads, modelnums = np.zeros(nmodels), np.zeros(nmodels), \
                                np.zeros(nmodels),
     for i in range(nmodels):
-        self = loadpickle(pickles[i])
-        modelnums[i] = int(pickles[i][-1])
+        self = selfs[i]
+        modelnums[i] = self.model
         medians[i] = np.median(self.lnlikes)
         mads[i] = MAD(self.lnlikes) / np.sqrt(self.lnlikes.size)
 
@@ -32,6 +39,35 @@ def plot_medians(datanum):
     ax.errorbar(modelnums, medians, mads, fmt='k.', capsize=0)
     ax.set_xlabel('Model Number'), ax.set_title('RVdata%.4d'%datanum)
     ax.set_ylabel('Median ln likelihood per measurement')
+    ax.set_xlim((-.5,3.5))
+    plt.show()
+    plt.close('all')
+
+
+def get_summed_lnlikes(datanum):
+    # Get results for this dataset
+    selfs = get_results(datanum)
+    nmodels = len(selfs)
+    
+    # Save summed lnlikelihoods
+    total_ll, modelnums = np.zeros(nmodels), np.zeros(nmodels)
+    for i in range(nmodels):
+        self = selfs[i]
+        modelnums[i] = self.model
+        total_ll[i] = self.lnlikes.sum()
+
+    # Sort by model number
+    sort = np.argsort(modelnums)
+    return modelnums[sort], total_ll[sort]
+
+
+def plot_total_lnlikes(datanum):
+    modelnums, total_ll = get_summed_results(datanum)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.errorbar(modelnums, total_ll, fmt='k.', capsize=0)
+    ax.set_xlabel('Model Number'), ax.set_title('RVdata%.4d'%datanum)
+    ax.set_ylabel('Total ln likelihood for %i measurements'%self.npickles)
     ax.set_xlim((-.5,3.5))
     plt.show()
     plt.close('all')
