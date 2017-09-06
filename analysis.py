37219a8a644121a0318c12ecca06d61a4d55fb2b
Ryan Cloutier <cloutier@astro.utoronto.ca>
2017-09-06 14:47:53 -0400

    wrote some analysis functions to plot the median lnlikelihood for each model

---
+++ analysis.py
@@ -0,0 +1,37 @@
+from imports import *
+from savepickle import *
+
+
+def MAD(arr):
+    return np.median(abs(arr - np.median(arr)))
+    
+
+def get_median_results(datanum):
+    # Get results for this dataset
+    pickles = np.array(glob.glob('results/RVdata%.4d/qsub_*'%datanum))
+    nmodels = pickles.size
+
+    # Save median lnlikelihoods
+    medians, mads, modelnums = np.zeros(nmodels), np.zeros(nmodels), \
+                               np.zeros(nmodels),
+    for i in range(nmodels):
+        self = loadpickle(pickles[i])
+        modelnums[i] = int(pickles[i][-1])
+        medians[i] = np.median(self.lnlikes)
+        mads[i] = MAD(self.lnlikes) / np.sqrt(self.lnlikes.size)
+
+    # Sort by model number
+    sort = np.argsort(modelnums)
+    modelnums, medians, mads = modelnums[sort], medians[sort], mads[sort]
+
+    return modelnums, medians, mads
+
+
+def plot_medians(datanum):
+    modelnums, medians, mads = get_median_results(datanum)
+    fig = plt.figure()
+    ax.errorbar(modelnums, medians, mads, fmt='k.', capsize=0)
+    ax.set_xlabel('Model Number'), ax.set_title('RVdata%.4d'%datanum)
+    ax.set_ylabel('Median ln likelihood per measurement')
+    plt.show()
+    plt.close('all')
