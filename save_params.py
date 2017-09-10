7c2b1f6759305e470eaca319364facdede6e34e5
Ryan Cloutier <cloutier@astro.utoronto.ca>
2017-09-09 22:49:37 -0400

    save 'evidences' rather median lnLs for each RVdataset other than 0002

--- save_params.py
+++ save_params.py
@@ -1,10 +1,13 @@
 from savepickle import *
 from run_MCMC import get_results_kernel
+from CVmodelcomparison_qsubjob import preferred_model
 
 
 def eo2hk(e,o):
     return e*np.cos(o), e*np.sin(o)
 
+def MAD(arr):
+    return np.median(abs(arr - np.median(arr)))
 
 def save_params(datanum, modelnum):
     self = loadpickle('results/RVdata%.4d/qsub_datanum%i_modelnum%i'%(datanum,
@@ -58,3 +61,43 @@ def save_params(datanum, modelnum):
     h = open('Cloutier/TimeSeriesCV/params_%i_%.4d.txt'%(modelnum, datanum),'w')
     h.write(g)
     h.close()
+
+
+def save_evidences(datanum):
+    pickles = np.sort(glob.glob('results/RVdata%.4d/qsub_*'%datanum))
+
+    g = '# Number of lnL evaluations, number of planets, median lnL per measurement, median absolute deviation of the median lnL per measurement\n'
+
+    models, lls, ells = np.zeros(pickles.size), np.zeros(pickles.size), \
+                        np.zeros(pickles.size)
+    for i in range(pickles.size):
+        self = loadpickle(pickles[i])
+        g += '1.6E+07,%i,'%self.model
+        models[i] = self.model 
+        g += '%.8e,%.8e\n'%(np.median(self.lnlikes),
+                            MAD(self.lnlikes) / np.sqrt(self.lnlikes.size))
+        lls[i], ells[i] = np.median(self.lnlikes), \
+                          MAD(self.lnlikes) / np.sqrt(self.lnlikes.size)
+
+    fname = 'Cloutier/TimeSeriesCV/evidences_%.4d.txt'%datanum
+    h = open(fname,'w')
+    h.write(g)
+    h.close()
+
+    # Get favoured model
+    '''winners = np.zeros(models.size-1)
+    for i in range(models.size-1):
+        winners[i] = float(preferred_model(models[i:i+2], lls[i:i+2],
+                                           ells[i:i+2]))
+    winner = int(winners.max())
+
+    f = open(fname, 'r')
+    g = f.read()
+    f.close()
+
+    g = g.replace('deviation of the median lnL per measurement\n',
+                  'deviation of the median lnL per measurement\n#Favoured model: %i planets\n'%winner)
+
+    h = open(fname,'w')
+    h.write(g)
+    h.close()'''
